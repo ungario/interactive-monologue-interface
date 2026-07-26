079f825511f75e748a987920565a7a8ed60a524f
umkl <michael.ungar03@gmail.com>
2026-07-26 19:50:19 +0200

    fix icons

---
+++ src/comps/DownloadIcon.tsx
@@ -0,0 +1,8 @@
+import { ComponentProps } from "react";
+
+export function DownloadIcon(props: ComponentProps<"svg">) {
+    return <svg aria-hidden="true" width="16" height="16" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path d="M8.751 0.75a.75.75 0 0 0-1.5 0v8.614l-2.955-3.129a.75.75 0 0 0-1.092 1.03l4.25 4.5a.75.75 0 0 0 1.092 0l4.25-4.5a.75.75 0 0 0-1.092-1.03L8.75 9.364V.75Z" />
+        <path d="M1.5 10.75a.75.75 0 0 0-1.5 0v2.5A2.75 2.75 0 0 0 2.75 16h10.5A2.75 2.75 0 0 0 16 13.25v-2.5a.75.75 0 0 0-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H2.75c-.69 0-1.25-.56-1.25-1.25v-2.5Z" />
+    </svg>
+}
